e03400ce09cbef1c1b736afe67f2bd69e3191a62
Geonu Lim <44187187+rjsdn0124@users.noreply.github.com>
2023-05-30 11:25:27 -0700

    :bug: 데모 전 마지막 커밋 (#7)

--- src/components/components/ModulePerformanceBox.js
+++ src/components/components/ModulePerformanceBox.js
@@ -1,26 +1,31 @@
-import { useEffect, useState } from "react";
-import { getProcessResult } from "../modules/getProcessResult";
 import { PercentageComponent } from "./PercentageComponent";
 import { CustomWhiteText } from "../atoms/CustomWhiteText";
 import { LoadingComponents } from "./LoadingComponent";
+import { BsDot } from "react-icons/bs";
 
-export const ModulePerformanceBox = ({ result: model_performance }) => {
-  if (!!model_performance)
-    return (
-      <div>
-        <CustomWhiteText style={{ fontSize: "26px" }}>
-          Model Performance
+export const ModulePerformanceBox = ({ selectedModule }) => {
+  const perform = [
+    { name: "FANet", MDS: "0.5392", ME: "0.9052" },
+    { name: "REDNet", MDS: "0.4987", ME: "1.0229" },
+    { name: "UNet", MDS: "0.2928", ME: "0.0124" },
+  ];
+  selectedModule = selectedModule?.slice(9, -4);
+  const now = perform.filter((p) => selectedModule === p.name)[0];
+  return (
+    <div>
+      <CustomWhiteText style={{ fontSize: "26px" }}>
+        Current Model Performance
+      </CustomWhiteText>
+      <LoadingComponents condition={selectedModule}>
+        <PercentageComponent
+          title="Mean Dice Score"
+          value={Math.round(now?.MDS * 100)}
+        />
+        <CustomWhiteText>
+          <BsDot />
+          Mean Execution Time : {now?.ME} sec
         </CustomWhiteText>
-        <LoadingComponents condition={model_performance}>
-          <PercentageComponent
-            title="Accuracy"
-            value={model_performance?.accuracy}
-          />
-          <CustomWhiteText>
-            Inference Time {model_performance?.inference_time}
-          </CustomWhiteText>
-        </LoadingComponents>
-      </div>
-    );
-  else return <div />;
+      </LoadingComponents>
+    </div>
+  );
 };
--- src/components/templates/ModuleInfoBox.js
+++ src/components/templates/ModuleInfoBox.js
@@ -1,12 +1,17 @@
 import { ModuleDropdown } from "./ModuleDropdown";
 import { ModulePerformanceBox } from "../components/ModulePerformanceBox";
 import { FlexPushBox } from "../atoms/FlexPushBox";
+import { useState } from "react";
 
 export const ModuleInfoBox = ({ result }) => {
+  const [selectedModule, setSelectedModule] = useState();
   return (
     <FlexPushBox>
-      <ModulePerformanceBox result={result} />
-      <ModuleDropdown />
+      <ModulePerformanceBox selectedModule={selectedModule} />
+      <ModuleDropdown
+        selectedModule={selectedModule}
+        setSelectedModule={setSelectedModule}
+      />
     </FlexPushBox>
   );
 };
